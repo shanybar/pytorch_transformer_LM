b4ce3310a51b0e4d4db3decdcb089a231ed66bc7
shanybar <shanyb21@gmail.com>
2023-01-09 16:41:08 +0200

    start adding models

--- src/models.py
+++ src/models.py
@@ -8,3 +8,6 @@ from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.utils.data import dataset
 
 class TransformerLM(nn.Module):
+    def __init__(self, vocab_size: int, d_model: int, heads_num: int, d_hid: int,
+                 layers_num: int, dropout:float=0.5):
+        pass
