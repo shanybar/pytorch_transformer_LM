62ae511fc842c1936ad431a1ed657d166df420fe
shanybar <shanyb21@gmail.com>
2023-01-10 12:14:58 +0200

    adding transformer LM methods

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ import math
 from typing import Tuple
 
 import torch
-from torch import nn
+from torch import nn, Tensor
 import torch.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.utils.data import dataset
@@ -27,6 +27,30 @@ class TransformerLM(nn.Module):
         self.embed_encoder.weight.data.uniform_(-init_range, init_range)
         self.linear_decoder.bias.data.zero_()
         self.linear_decoder.weight.data.uniform_(-init_range, init_range)
+
+    def forward(self, src: Tensor, src_mask: Tensor) -> Tensor:
+        """
+
+        :param src: Tensor, shape [seq_len, batch_size]
+        :param src_mask: Tensor, shape [seq_len, seq_len]
+
+        :return: output Tensor of shape [seq_len, batch_size, vocab_size]
+        """
+        src = self.embed_encoder(src)
+        src *= math.sqrt(self.d_model)
+        src = self.position_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.linear_decoder(output)
+        return output
+
+
+def generate_mask_matrix(size: int) -> Tensor:
+    """
+    Generates an upper-triangular matrix of -inf, with zeros on diag.
+    :param size: matrix size
+    :return: mask tensor
+    """
+    return torch.triu(torch.ones(size,size) * float('-inf'), diagonal=1)
 
 
 class PositionalEncoding(nn.Module):
