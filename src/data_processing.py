a1c564c16ca36474629a2991113d8deb5654d8ab
shanybar <shanyb21@gmail.com>
2023-01-12 14:01:06 +0200

    strat adding data loading and pre-processing

---
+++ src/data_processing.py
@@ -0,0 +1,12 @@
+
+from torchtext.datasets import WikiText2
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+
+
+def load_data():
+    train_iter = WikiText2(split='train')
+    tokenizer = get_tokenizer('basic_english')
+    vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
+    vocab.set_default_index(vocab['<unk>'])
+    
