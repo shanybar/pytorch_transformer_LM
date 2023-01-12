7f63d0688c7553f67284c93bf3057e3276398d21
shanybar <shanyb21@gmail.com>
2023-01-12 13:05:50 +0200

    adding positional encoding

--- src/models.py
+++ src/models.py
@@ -54,4 +54,21 @@ def generate_mask_matrix(size: int) -> Tensor:
 
 
 class PositionalEncoding(nn.Module):
-    pass
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len:int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0,d_model,2) * (-math.log(10000.0)/d_model))
+        position_embeds = torch.zeros(max_len, 1, d_model)
+        position_embeds[:, 0, 0::2] = torch.sin(position * div_term)
+        position_embeds[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('position_embeds', position_embeds) # Saved to state_dict
+        # but stayed fixed and won't be updated by the optimizer
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        :param x: Tensor, shape [seq_len, batch_size, embeddings_dim]
+        :return: the tensor x summed with the position embeddings
+        """
+        x = x + self.position_embeddings[:x.size(0)]
+        return self.dropout(x)
