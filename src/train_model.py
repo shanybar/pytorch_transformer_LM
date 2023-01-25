0d892e5d3ab8e65bdf3125b19d4866be612f6d4a
shanybar <shanyb21@gmail.com>
2023-01-25 11:48:54 +0200

    Finishing implementation

--- src/train_model.py
+++ src/train_model.py
@@ -1,3 +1,4 @@
+import math
 import time
 import copy
 import torch
@@ -32,16 +33,37 @@ def train_model():
 
     for epoch in range(1, epochs+1):
         epoch_start_time = time.time()
-        train_single_epoch(model, train_data, optimizer, scheduler, loss_func, bptt, num_tokens)
+        train_single_epoch(model, train_data, optimizer, scheduler, loss_func, bptt,
+                           num_tokens, epoch, device)
+        val_loss = eval_model(model,val_data,loss_func, bptt, num_tokens, device)
+        val_perplexity = math.exp(val_loss)
+        elapsed = time.time() - epoch_start_time
+        print('-' * 89)
+        print(f'| end of epoch: {epoch:3d} | time: {elapsed:5.2f}s |'
+              f' val loss {val_loss: 5.2f} | val perplexity {val_perplexity: 8.2f}')
+        print('-' * 89)
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_model = copy.deepcopy(model)
+
+        scheduler.step()
+
+    test_loss = eval_model(best_model, test_data, loss_func, bptt, num_tokens, device)
+    test_perplexity = math.exp(test_loss)
+    print('=' * 89)
+    print(f'End of training | test loss: {test_loss: 5.2f} | '
+          f'test perplexity: {test_perplexity: 8.2f}')
+    print('=' * 89)
 
 
 def train_single_epoch(model: nn.Module, train_data, optimizer, scheduler, loss_func,
-                       bptt: int, num_tokens: int) -> None:
+                       bptt: int, num_tokens: int, epoch, device) -> None:
     model.train() # turn on train mode
     total_loss = .0
-    log_interval = 100
+    log_interval = 200
     start_time = time.time()
-    src_mask = generate_mask_matrix(bptt)
+    src_mask = generate_mask_matrix(bptt).to(device)
 
     num_batches = len(train_data) // bptt
     for batch, i in enumerate(range(0, train_data.size(0) - 1), bptt):
@@ -59,8 +81,31 @@ def train_single_epoch(model: nn.Module, train_data, optimizer, scheduler, loss_
 
         total_loss += loss.item()
         if batch % log_interval == 0 and batch > 0:
-            
+            last_lr = scheduler.get_last_lr()[0]
+            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            curr_loss = total_loss / log_interval
+            perplexity = math.exp(curr_loss)
+            print(f'| epoch: {epoch:3d} | batch: {batch:5d}/{num_batches:5d}'
+                  f'| lr: {last_lr:02.2f} | ms/batch: {ms_per_batch:5.2f}'
+                  f'| loss: {curr_loss:5.2f | perplexity: {perplexity: 8.2f}}')
+            total_loss = 0
+            start_time = time.time()
 
 
-def eval_model():
-    pass
+def eval_model(model: nn.Module, eval_data, loss_func,
+               bptt: int, num_tokens: int, device) -> float:
+    model.eval()
+    total_loss = 0.
+    src_mask = generate_mask_matrix(bptt).to(device)
+
+    with torch.no_grad:
+        for i in range(0, eval_data.size(0) - 1, bptt):
+            data, labels = get_batch(eval_data, i, bptt)
+            seq_len = data.size(0)
+            if seq_len != bptt:  # only for the last batch
+                src_mask = src_mask[:seq_len, :seq_len]
+            output = model(data, src_mask)
+            output_flat = output.view(-1, num_tokens)
+            total_loss += seq_len * loss_func(output_flat, labels).item()
+
+    return total_loss / len(eval_data)
